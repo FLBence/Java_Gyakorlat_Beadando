3aaa0a71a9d751054a0385d9fea5bbb4bac10c5b
FLBence <bence2405.fb@gmail.com>
2025-11-25 17:46:11 +0100

    reszponzív téma hozzáadása

---
+++ src/main/java/com/example/java_gyak_bead/JavaGyakBeadApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_gyak_bead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGyakBeadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGyakBeadApplication.class, args);
+    }
+
+}
